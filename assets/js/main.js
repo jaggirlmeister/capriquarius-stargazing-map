922c7c782e423cb5ee65ac09aa45e59a715208be
rodriandrade <rodriandrade1498@gmail.com>
2020-05-14 10:18:11 -0300

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,7 +124,7 @@ window.initMap = () => {
 //Función de asincrónica que trae los markers
 const fetchMarkers = async (map) => { 
     try {
-        const response = await fetch('assets/data/markers.json');
+        const response = await fetch('https://stargazing-map-api-rest.now.sh/locations');
         const json = await response.json();
         json.forEach(marker => {
             addMarker(map, marker);
@@ -138,7 +138,7 @@ const fetchMarkers = async (map) => {
 //Función de agregado de un marker
 const addMarker = (map, marker) => { 
     //Destructuring de la info del marker
-    const { lat, lng, name, country, img, link, type } = marker;
+    const { lat, lng, name, country, img, link, description, type } = marker;
 
     //Armo la infowindow
     const contentString = `
@@ -149,10 +149,10 @@ const addMarker = (map, marker) => {
         <div class='infoPlace'>
             <h2>${name}</h2>
             <h3>${type}</h3>
-            <a href="${link}" target="_blank">More information</a>
+            <a href="${link}" target="_blank">Website</a>
         </div>
-        <button>More Information</button>
     </div>`;
+    
     const infowindow = new google.maps.InfoWindow({
         content: contentString
     });
@@ -172,7 +172,7 @@ const addMarker = (map, marker) => {
     //Agrego el marker
     const markerItem = new google.maps.Marker(
         {
-            position: { lat: lat, lng: lng },
+            position: { lat: parseFloat(lat), lng: parseFloat(lng) },
             icon: icons[type],
             map: map,
             customInfo: type,
@@ -183,6 +183,7 @@ const addMarker = (map, marker) => {
     markerItem.setMap(map);
 
     //Styling map with jQuery
+    
     const styleWindow = () =>{ 
         const infoWindowEdit = $('.thisWindowHook').parent().parent().parent();
             infoWindowEdit.css({
@@ -198,38 +199,7 @@ const addMarker = (map, marker) => {
                 'padding-bottom' : '10px'
             })
         }
-
-    const locationInfo = () =>{
-        const filters = document.querySelector('#filtersMenu');
-        const info = document.querySelector('#locationInfo');
         
-        
-        filters.classList.add('hide');
-        info.classList.remove('hide');
-
-        info.innerHTML = `
-        <div id="back" class="button backBtn">
-            <img id="backBtn" class="arrow" src="assets/images/left-arrow-01.svg">
-        </div>
-        
-        <div class='imgCont'>
-            <img src="${img}">
-        </div>
-        <div class='infoPlace'>
-            <h2>${name}</h2>
-            <h3>${type}</h3>
-            <a href="${link}" target="_blank">More information</a>
-        </div>
-
-        `;
-
-        const back = document.querySelector('#back');
-
-        back.addEventListener('click', () => {
-            filters.classList.remove('hide');
-            info.classList.add('hide');
-        });
-    }
         
     //Agrego evento de click en el marker, abre infowindow y cierra los demás
         
@@ -239,13 +209,160 @@ const addMarker = (map, marker) => {
         })
         infowindow.open(map, markerItem);
             styleWindow();
-            locationInfo();
+            locationInfo(marker);
         });
             
     //Agrego mi nuevo marker (objeto marker, no json marker, a mi array para filtros)
     markersAll.push(markerItem);
 }
-        
+
+//Función que muestra la información del marker en el panel 
+const locationInfo = (marker) =>{
+    const { lat, lng, name, country, img, link, description, type } = marker;
+    const filters = document.querySelector('#filtersMenu');
+    const info = document.querySelector('#locationInfo');
+
+    filters.classList.add('hide');
+    info.classList.remove('hide');
+
+    info.innerHTML = `
+    <div id="back" class="button backBtn">
+        <img id="backBtn" class="arrow" src="assets/images/left-arrow-01.svg">
+    </div>
+    
+    <div class='imgCont'>
+        <img src="${img}">
+    </div>
+    <div class='infoPlace'>
+        <h2 id="placeTitle">${name}</h2>
+        <div id="filterWeather">
+            <h3 id="weatherConditionSub" class="subtitle">Weather condition</h3>
+            <div id="selectHour">
+                <select id="pickHour" onchange="showData(${lat}, ${lng})">
+                    <option value="0" selected>00:00hs</option>
+                    <option value="1">03:00hs</option>
+                    <option value="2">06:00hs</option>
+                    <option value="3">09:00hs</option>
+                    <option value="4">12:00hs</option>
+                    <option value="5">15:00hs</option>
+                    <option value="6">18:00hs</option>
+                    <option value="7">21:00hs</option>
+                </select>
+            </div>
+        </div>
+        <div id="weatherCondition">
+            <img src="assets/images/loading.gif" width="70px">
+        </div>
+        <h3 class="subtitle" id="desc">Description</h3>
+        <p id="placeDescription">${description}</p>
+        <a href="${link}" target="_blank" id="placeLink">Website</a>
+    </div>
+   
+
+    `;
+
+    const back = document.querySelector('#back');
+
+    back.addEventListener('click', () => {
+        filters.classList.remove('hide');
+        info.classList.add('hide');
+    });
+
+    showData(lat,lng);
+}
+
+// Función que trae los datos de la API openweather
+const fetchForecast = async (lat, lng) => {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"; // Proxy que permite que se pueda hacer el fetch, tira error de CORS sin esto. 
+    const url = 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lng+'&units=metric&appid=0e3498f3359f3fe49fd95d453c4c9a8c';
+    const response = await fetch(proxyurl + url);
+    const json = await response.json();
+    const {list} = json;
+    console.log(url);
+    return list
+}
+
+// Función que muestra los datos de la API openweather
+const showData = async (lat, lng) =>{ 
+    const weatherData = await fetchForecast(lat, lng);
+    const weatherCondition = document.querySelector('#weatherCondition');
+
+    let cloudsAmount = [];
+    let transparencySky = [];
+    let temperature = [];
+    let date = [];
+    let time = document.querySelector('#pickHour');
+    let timeValue = time.value;
+    
+    for(i=0;i<8;i++){
+        cloudsAmount.push(weatherData[i].clouds.all);
+        transparencySky.push(weatherData[i].main.humidity);
+        temperature.push(weatherData[i].main.temp);
+        date.push(weatherData[i].dt_txt);
+    }
+
+    /*
+    for(i=0;i<weatherData.length;i++){
+        date.push(weatherData[i].dt_txt);
+    } */
+    
+    console.log(date)
+
+    weatherCondition.innerHTML = `
+    <div>
+        <p class="weatherValue">${cloudsAmount[timeValue]} %</p>
+        <p class="weatherData">Cloud Cover</p>
+    </div>
+    <div>
+        <p class="weatherValue">${transparencySky[timeValue]} %</p>
+        <p class="weatherData">Humidity</p>
+    </div>
+    <div>
+        <p class="weatherValue">${temperature[timeValue]} C°</p>
+        <p class="weatherData">Temperature</p>
+    </div>
+    `
+}
+
+/*
+
+const fetchForecast = async (lng, lat) => {
+    const response = await fetch('http://www.7timer.info/bin/astro.php?lon='+lng+'&lat='+lat+'&ac=0&unit=metric&output=json&tzshift=0');
+    const json = await response.json();
+    const {dataseries} = json;
+    return dataseries;
+}
+
+const showData = async (lat, lng) =>{
+    const weatherData = await fetchForecast(lat, lng);
+
+    const cloudAmount = weatherData.map(({cloudcover}) => cloudcover);
+    const transparencySky = weatherData.map(({transparency}) => transparency);
+    const seeingSky = weatherData.map(({seeing}) => seeing);
+
+    console.log(cloudAmount)
+    console.log(transparencySky)
+    console.log(seeingSky)
+
+    const weatherCondition = document.querySelector('#weatherCondition');
+
+    weatherCondition.innerHTML = `
+    <div>
+        <p class="weatherData">${cloudAmount[0]}</p>
+        <p class="weatherData">Cloud Cover</p>
+    </div>
+    <div>
+        <p class="weatherData">${transparencySky[0]}</p>
+        <p class="weatherData">Transparency</p>
+    </div>
+    <div>
+        <p class="weatherData">${seeingSky[0]}</p>
+        <p class="weatherData">Seeing</p>
+    </div>
+    `
+}
+
+*/
         
 //jQuery for dropdown
         
