e06aae0c22360c9cbb04c64c1b029730e1198070
rodriandrade <rodriandrade1498@gmail.com>
2020-05-24 07:21:29 -0300

    chevron down

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -9,21 +9,29 @@ const $form_field_type = document.querySelector('#form_field_type');
 const $form_field_name = document.querySelector('#form_field_name');
 
 const dataRow = props => {
-    const {_id, lat, lng, name, country, type} = props
+    const {_id, lat, lng, name, country, type, description} = props
 
     return `
         <div class="item">
-            <div class="list_content">
-            <div>
-                <h2>${name}</h2>
-            </div>
             <div class="countriesAdmin">
-                <img class="flags" src="${flags[country]}"><p class="countryAdmin">${country}</p>
-            </div>
+                <div class="countriesAdmin">
+                    <img class="down" src="assets/images/down.svg" width="25px">
+                </div>
+                <div class="list_content">
+                    <div>
+                        <h2>${name}</h2>
+                    </div>
+                    <div class="countriesAdmin">
+                        <img class="flags" src="${flags[country]}"><p class="countryAdmin">${country}</p>
+                    </div>
+                </div>
             </div>
             <div class="btns_wrapper">
                 <a href="#" data-id="${_id}" class="red btn handleEdit">Edit</a>
                 <a href="#" data-id="${_id}" class="green btn handleDelete">Delete</a>
+            </div>
+            <div class="adminDescription hide">
+                <p>${description}</p>
             </div>
         </div>
     `
