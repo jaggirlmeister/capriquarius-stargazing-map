3df1b7dbef0c3ebf1b34647ddad292a75a5a00d4
Nayla <nayla.mar2@gmail.com>
2020-05-28 11:17:52 -0300

    api.js - POST and PUT methods added

--- utils/api.js
+++ utils/api.js
@@ -17,7 +17,15 @@ const fetchParams = (method, data = '') =>{
 }
 
 const api = {
-    //GET
+    //Funciones CRUD
+    //CREATE (POST)
+    createLocation: async formData => {
+        const dataResponse = await fetch(baseUrl + 'locations', fetchParams('POST', formData));
+        const dataInfo = await dataResponse.json();
+        return dataInfo;
+    },
+    
+    //READ (GET)
     getLocations: async() => {
         const dataResponse = await fetch(baseUrl + 'locations', fetchParams('GET'));
         const dataInfo = await dataResponse.json();
@@ -25,17 +33,17 @@ const api = {
         return dataInfo;
     },
 
-    //DELETE
+    //UPDATE (PUT)
+    updateLocations: async (formData,id) => {
+        const dataResponse = await fetch(baseUrl + 'location/' + id, fetchParams('PUT', formData));
+        const dataInfo = await dataResponse.json();
+        return dataInfo;
+    },
 
+    //DELETE
     deleteLocations: async id => {
         const dataResponse = await fetch(baseUrl + 'location/' + id, fetchParams('DELETE'));
         const dataInfo = await dataResponse.json();
-
         return dataInfo;
     }
-
-
-    //PUT
-
-    //POST
 }
